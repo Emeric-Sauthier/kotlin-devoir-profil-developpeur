3d91668715732a7e3db06f68fd850077cee1949a
Emeric Sauthier <e.sauthier@myskolae.fr>
2026-07-08 12:18:56 +0200

    Add Icon + delete unused images

--- app/src/main/java/com/example/profildeveloppeur/components/ProfileContactButton.kt
+++ app/src/main/java/com/example/profildeveloppeur/components/ProfileContactButton.kt
@@ -1,11 +1,16 @@
 package com.example.profildeveloppeur.components
 
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Button
+import androidx.compose.material3.Icon
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Call
+import androidx.compose.ui.Alignment
 
 @Composable
 fun ProfileContactButton(
@@ -17,8 +22,18 @@ fun ProfileContactButton(
         onClick = onContactClick,
         modifier = modifier.padding(10.dp)
     ) {
-        Text(
-            text = "☎  $contact"
-        )
+        Row(
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Icon(
+                imageVector = Icons.Default.Call,
+                contentDescription = "Bouton de contact"
+            )
+
+            Text(
+                text = contact,
+                modifier = Modifier.padding(start = 4.dp)
+            )
+        }
     }
 }
